d595ff2be5fc2bec7ee1a77b79a29bfa02be0a59
TrevourGuild <trevorr.guildd@gmail.com>
2021-09-01 13:48:38 -0700

    more than half tests are passing!

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,24 +1,58 @@
-const express = require('express');
+const User = require('../users/users-model')
 
-const server = express();
 
 function logger(req, res, next){
-  console.log('this is happening')
   console.log(`it is a ${req.method} request!`)
   next()
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+  try{
+    const user = await User.getById(req.params.id)
+    if (!user){
+      res.status(404).json({
+        message: 'no such user'
+      })
+    }else{
+      req.user = user
+      next()
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'problem finding user'
+    })
+  }
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  const { name } = req.body
+  if (!name || !name.trim()){
+    res.status(400).json({
+      message: 'Missing required name field'
+    })
+  }else{
+    req.name = name.trim()
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  const { text } = req.body
+  if (!text || !text.trim()){
+    res.status(400).json({
+      message: 'Missing required text field'
+    })
+  }else{
+    req.text = text.trim()
+    next()
+  }
 }
 
 // do not forget to expose these functions to other modules
-server.use(logger)
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,6 +1,13 @@
 const express = require('express');
-const Users = require('./users-model')
-const Posts = require('../posts/posts-model')
+
+const {
+  validateUserId,
+  validateUser,
+  validatePost
+} = require('../middleware/middleware')
+
+const User = require('./users-model')
+const Post = require('../posts/posts-model')
 
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
@@ -9,25 +16,28 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   // RETURN AN ARRAY WITH ALL THE USERS
-  res.json('array of users')
+  User.get()
+  .then(users =>{
+    res.json(users)
+  })
   .catch(next)
 });
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', validateUserId, (req, res, next) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
   res.json('user object')
   .catch(next)
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', validateUser,(req, res, next) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
   res.json('newly created user')
   .catch(next)
 });
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', validateUserId, validateUser,(req, res, next) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
@@ -35,30 +45,31 @@ router.put('/:id', (req, res, next) => {
   .catch(next)
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', validateUserId, (req, res, next) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
   res.json('delete user')
   .catch(next)
 });
 
-router.get('/:id/posts', (req, res, next) => {
+router.get('/:id/posts', validateUserId, (req, res, next) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
   res.json('arry of posts')
   .catch(next)
 });
 
-router.post('/:id/posts', (req, res, next) => {
+router.post('/:id/posts', validateUserId, validatePost,(req, res, next) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
-  res.json('new post')
+  console.log(req.user)
+  console.log(req.text)
   .catch(next)
 
 });
 
-router.use((err, req, res, next) =>{
+router.use((err, req, res, next) =>{ //eslint-disable-line
   res.status(err.status || 500).json({
     message:err.message,
     customMessage: 'Something bad inside the hubs router!'
